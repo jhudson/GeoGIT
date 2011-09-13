48fffac651bd1d1be8254e6bab2fdbc259cd4dff
Mark Leslie <mark.s.leslie@gmail.com>
2011-09-13 15:28:49 +0930

    Rewriting the hessian blob printer to output xml for commit printing.

--- src/main/java/org/geogit/storage/hessian/HessianBlobPrinter.java
+++ src/main/java/org/geogit/storage/hessian/HessianBlobPrinter.java
@@ -5,15 +5,26 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
 
 import org.geogit.api.BlobPrinter;
+import org.geogit.api.ObjectId;
 
 import com.caucho.hessian.io.Hessian2Input;
 
 public class HessianBlobPrinter 
 		extends HessianRevReader 
 		implements BlobPrinter {
+	
+	Stack<EntityState> entityStack;
+	
+	public HessianBlobPrinter() {
+		entityStack = new Stack<EntityState>();	
+	}
 
 	@Override
 	public void print(byte[] rawBlob, PrintStream out) throws IOException {
@@ -54,17 +65,125 @@ public class HessianBlobPrinter
 		
 	}
 	
-	private void printCommit(Hessian2Input hin, PrintStream out) throws IOException {
-		out.println("Commit");
-		out.println("\tTreeId - " + readObjectId(hin));
-		int parentCount = hin.readInt();
-		out.println("\tParents");
-		for(int i = 0; i < parentCount; i++) {
-			out.println("\t\t- " + readObjectId(hin));
+	private class EntityState {
+		boolean wrap;
+		String entityName;
+		
+		public EntityState(String entityName, boolean wrap) {
+			this.entityName = entityName;
+			this.wrap = wrap;
 		}
-		out.println("\tAuthor - " + hin.readString());
-		out.println("\tCommitter - " + hin.readString());
-		out.println("\tMessage - " + hin.readString());
-		out.println("\tTimestamp - " + hin.readLong());
+	}
+	
+	private void printCommit(Hessian2Input hin, PrintStream out) throws IOException {
+		Map<String, String> attrMap = new HashMap<String, String>();
+		attrMap.put("xmlns", null);
+		openTag("commit", attrMap, out, true, false);
+		openTag("tree", out);
+		printObjectId(readObjectId(hin), out);
+		closeTag(out);
+		int parentCount = hin.readInt();
+		openTag("parentids", out);
+		for(int i = 0; i < parentCount; i++) {
+			printObjectId(readObjectId(hin), out);
+		}
+		closeTag(out);
+		openTag("author", out);
+		printString(hin.readString(), out);
+		closeTag(out);
+		openTag("committer", out);
+		printString(hin.readString(), out);
+		closeTag(out);
+		openTag("message", out);
+		printString(hin.readString(), out);
+		closeTag(out);
+		openTag("timestamp", out);
+		printLong(hin.readLong(), out);
+		closeTag(out);
+		closeTag(out);
+	}
+	
+	private void openTag(String entity, PrintStream out) throws IOException {
+		openTag(entity, out, true);
+	}
+	
+	private void openTag(String entity, PrintStream out, boolean wrap) throws IOException {
+		openTag(entity, out, wrap, false);
+	}
+	
+	private void openTag(String entity, PrintStream out, boolean wrap, boolean empty) throws IOException {
+		openTag(entity, null, out, wrap, empty);
+	}
+	
+	private void openTag(String entity, Map<String, String> attrs, PrintStream out, boolean wrap, boolean empty) throws IOException {
+		if(entityStack.size() > 0) {
+			EntityState lastState = entityStack.peek();
+			if(lastState.wrap)
+				printIndent(out);
+		}
+		out.print("<");
+		out.print(entity);
+		if(attrs != null) {
+			Set<String> keys = attrs.keySet();
+			for(String key : keys) {
+				String value = attrs.get(key);
+				out.print(" ");
+				out.print(key);
+				out.print("=\"");
+				if(value != null)
+					out.print(value);
+				else
+				out.print("\"");
+			}
+		}
+		if(empty) {
+			out.print("/>");
+		} else {
+			out.print(">");
+			entityStack.push(new EntityState(entity, wrap));
+		}
+		if(wrap)
+			out.print("\n");
+	}
+	
+	private void closeTag(PrintStream out) throws IOException {
+		EntityState entity = entityStack.pop();
+		if(entity.wrap) {
+			out.print("\n");
+			printIndent(out);
+		}
+		out.print("</");
+		out.print(entity.entityName);
+		out.print(">");
+		if(entity.wrap)
+			out.print("\n");
+	}
+	
+	private void printIndent(PrintStream out) throws IOException {
+		for(int i = 0; i < entityStack.size(); i++) {
+			out.print(" ");
+		}
+	}
+	
+	private void printObjectId(ObjectId value, PrintStream out) throws IOException {
+		openTag("objectid", out, false);
+		out.print(value.toString());
+		closeTag(out);
+	}
+	
+	private void printLong(long value, PrintStream out) throws IOException {
+		openTag("long", out, false);
+		out.print(value);
+		closeTag(out);
+	}
+	
+	private void printString(String value, PrintStream out) throws IOException {
+		if(value == null) {
+			openTag("null", out, true, true);
+		} else {
+			openTag("string", out, false);
+			out.print(value);
+			closeTag(out);
+		}
 	}
 }
